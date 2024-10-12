29c7c960d0e70ae36ce257ef063e62521a54c5c7
Ecmosplasmidou <cassamb97@gmail.>
2024-10-12 21:48:32 +0200

    Debug True

--- jo_ticket/jo_ticket/production.py
+++ jo_ticket/jo_ticket/production.py
@@ -5,7 +5,7 @@ from decouple import config
 from pathlib import Path
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 DATABASES = {
     'default': dj_database_url.config(
