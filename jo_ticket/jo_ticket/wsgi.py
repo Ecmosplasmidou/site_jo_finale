2a0a58dcc563c23d444258295a3c072ac0423ab5
Ecmosplasmidou <cassamb97@gmail.>
2024-10-12 15:58:11 +0200

    deploie

--- jo_ticket/jo_ticket/wsgi.py
+++ jo_ticket/jo_ticket/wsgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jo_ticket.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jo_ticket.jo_ticket.settings")
 
 application = get_wsgi_application()
