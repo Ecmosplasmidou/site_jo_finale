8a1fe08e1cae90750454d7e74cb97252ecab00bb
Ecmosplasmidou <cassamb97@gmail.>
2024-10-12 18:56:34 +0200

    deploie

--- jo_ticket/jo_ticket/wsgi.py
+++ jo_ticket/jo_ticket/wsgi.py
@@ -2,6 +2,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 # Configurez l'environnement Django
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jo_ticket.jo_ticket.settings")  # Vérifiez que cela pointe vers le bon fichier settings.py
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jo_ticket.settings")  # Vérifiez que cela pointe vers le bon fichier settings.py
 
 application = get_wsgi_application()  # C'est ce qui sera importé par waitress
